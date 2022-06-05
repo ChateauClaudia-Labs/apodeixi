c897afa16d69e5c5fa5b128c335e40270b12574b
Alejandro Hernandez at CC Labs <alejandro@chateauclaudia-labs.com>
2022-06-05 12:33:59 -0700

    Fixed bug in toolchain for windows conda builds

--- toolchain/env.sh
+++ toolchain/env.sh
@@ -4,6 +4,8 @@ echo "[INFO] Setting environment variables"
 
 export PATH="${CCL_CHASSIS_HOME}/ccl-chassis/devops/bin":$PATH
 export _CFG__PIPELINE_ALBUM="${APODEIXI_DEVOPS_HOME}/apodeixi-devops/pipeline_album"
+export _CFG__PIPELINE_HISTORY="../apdo_builds"
 
-echo "[INFO] PATH                   = ${PATH}"
-echo "[INFO] _CFG__PIPELINE_ALBUM   = ${_CFG__PIPELINE_ALBUM}"
+echo "[INFO] PATH                       = ${PATH}"
+echo "[INFO] _CFG__PIPELINE_ALBUM       = ${_CFG__PIPELINE_ALBUM}"
+echo "[INFO] _CFG__PIPELINE_HISTORY     = ${_CFG__PIPELINE_HISTORY}"
